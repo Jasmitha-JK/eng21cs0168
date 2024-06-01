13ba21fc351c11472e237a56e6bc42b51509c8f3
jasmitha <jashjk82@gmail.com>
2024-06-01 12:00:01 +0530

    register

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+import "./App.css";
+  
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	const [data, setData] = useState(null);
+  const [post, setPostId] = useState(null);
+
+	useEffect(() => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				companyName: "skibidi",
+				ownerName: "jasmitha",
+				rollNo: "eng21cs0168",
+				ownerEmail: "eng21cs0168@dsu.edu.in",
+				accessCode: "sMYzGa",
+			}),
+		}
+    fetch('https://20.244.56.144/test/register', requestOptions)
+    .then(response => response.json())
+    .then(data => setPostId(data.id));
+	}, []);
+
+  
+	return (
+    <>
+    <div className="add-post-container">
+       <form>
+          <input type="text" className="form-control" />
+          <textarea className="form-control" cols="10" rows="8"></textarea>
+          <button type="submit">Add Post</button>
+       </form>
     </div>
-  );
-}
+    <div className="posts-container">
+       
+    </div>
+ </>
+);
+};
+
 
 export default App;
